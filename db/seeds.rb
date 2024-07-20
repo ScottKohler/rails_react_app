777cfc384b00ffb9b9eb4a412d8c0d1faccce75f
ScottKohler <scottdkohler@gmail.com>
2024-07-20 16:44:32 -0400

    Update seeds.rb
    reverted to 11

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 Post.destroy_all
 
 #create 20 posts
-21.times do
+11.times do
     Post.create(        
         title: Faker::Lorem.sentence(word_count: 3),
         body: Faker::Lorem.paragraph(sentence_count: 3)
